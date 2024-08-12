2e44f142e4fc5af88f033e82ddc0629d319f9795
Timur Makhmudov <mahmudov@lbr.ru>
2024-08-12 13:49:06 +0300

    0010: Добавил импортер курсов с бинанса

--- commands/RatesImportController.php
+++ commands/RatesImportController.php
@@ -2,15 +2,57 @@
 
 namespace app\commands;
 
+use app\models\Moment;
 use app\models\Pair;
 use app\models\Rate;
 use yii\base\InvalidConfigException;
 use yii\console\Controller;
+use yii\db\Query;
 use yii\httpclient\Client;
 use yii\httpclient\Exception;
 
 class RatesImportController extends Controller
 {
+    const START_DATE = "2023-07-01 00:00:00";
+
+    const END_DATE = "2024-06-30 23:59:59";
+
+    /**
+     * @throws \yii\db\Exception
+     */
+    public function actionMoments()
+    {
+        /**
+         * @var Moment $moment
+         */
+        $moment = Moment::find()
+            ->orderBy(['timestamp' => SORT_DESC])
+            ->one();
+
+        $currentTimestamp = !empty($moment)
+            ? $moment->timestamp + 60 : strtotime(self::START_DATE);
+
+        $batchData = [];
+
+        while ($currentTimestamp <= strtotime(self::END_DATE)) {
+            $batchData[] = [
+                'timestamp' => $currentTimestamp,
+                'is_current' => false,
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp,
+            ];
+
+            if (count($batchData) >= 1000) {
+                Moment::batchInsert($batchData);
+                $batchData = [];
+            }
+
+            $currentTimestamp += 60;
+        }
+
+        Moment::batchInsert($batchData);
+    }
+
     /**
      * @throws Exception
      * @throws InvalidConfigException
@@ -18,66 +60,69 @@ class RatesImportController extends Controller
      */
     public function actionRun()
     {
-        $pairs = Pair::find()->where(['>=', 'id', 186])->all();
+        $pairs = Pair::find()->where(['>=', 'id', 251])->all();
         $client = new Client(['baseUrl' => 'https://api.binance.com/api/v3/klines']);
 
-        $startTimestamp = strtotime('2023-07-01 00:00:00');
-        $endTimestamp = strtotime('2024-06-30 23:59:59');
-
-        $step = 15 * 60 * 60;
-
+        /**
+         * @var Pair $pair
+         */
         foreach ($pairs as $pair) {
-            $lastPairRateTimestamp = Rate::find()
-                ->select('created_at')
+
+            $momentQuery = (new Query())
+                ->from(Moment::tableName());
+
+            /**
+             * @var Rate $lastPairRateMoment
+             */
+            $lastPairRateMoment = Rate::find()
                 ->where(['pair_id' => $pair->id])
-                ->orderBy(['id' => SORT_DESC])
-                ->scalar();
+                ->orderBy(['moment_id' => SORT_DESC])
+                ->one();
 
-            $t = $lastPairRateTimestamp ? $lastPairRateTimestamp + 60 : $startTimestamp;
+            if ($lastPairRateMoment) {
+                $momentQuery->where(['>', 'id', $lastPairRateMoment->moment_id]);
+            }
 
-            while ($t < $endTimestamp) {
-                echo "Current: " . $t . "\n";
-                echo "End Timestamp: " . $endTimestamp . "\n\n";
+            $batchCount = 0;
 
-                $chunkLimit = $t + $step;
+            foreach ($momentQuery->batch(900) as $moments) {
+                $startTimestamp = $moments[0]['timestamp'];
+                $endTimestamp = $moments[count($moments) - 1]['timestamp'];
 
-                $response = $client->createRequest()
+                $ratesApiResponse = $client->createRequest()
                     ->setMethod('GET')
                     ->setData([
                         'symbol' => $pair->base_currency . $pair->quoted_currency,
                         'interval' => '1m',
-                        'startTime' => $t * 1000,
-                        'endTime' => $chunkLimit * 1000,
+                        'startTime' => $startTimestamp * 1000,
+                        'endTime' => $endTimestamp * 1000,
                         'limit' => 1000,
                     ])->send();
 
-                if (!$response->isOk) {
+                if (!$ratesApiResponse->isOk) {
                     throw new \Exception('Возникла ошибка');
                 }
 
+                $responseData = array_column($ratesApiResponse->getData(), null, 0);
                 $batchData = [];
-                $responseData = $response->getData();
 
-                foreach ($responseData as $rateItem) {
-                    $moment = ((int) $rateItem[0]) / 1000;
-
-                    if ($moment > $endTimestamp) {
-                        break;
-                    }
-
+                foreach ($moments as $moment) {
+                    $rateData = $responseData[$moment["timestamp"] * 1000] ?: null;
                     $batchData[] = [
                         'pair_id' => $pair->id,
-                        'rate' => $rateItem[1],
-                        'created_at' => $moment,
-                        'updated_at' => $moment,
+                        'moment_id' => $moment["id"],
+                        'rate' => !is_null($rateData) ? $rateData[1] : null,
+                        'created_at' => $moment["timestamp"],
+                        'updated_at' => $moment["timestamp"],
                     ];
                 }
 
-                \Yii::$app->db->createCommand()
-                    ->batchInsert(Rate::tableName(), ['pair_id', 'rate', 'created_at', 'updated_at'], $batchData)
-                    ->execute();
+                $batchCount += count($batchData);
 
-                $t = $chunkLimit + 60;
+                echo count($batchData) . "\n";
+                echo $batchCount . "\n\n";
+
+                Rate::batchInsert($batchData);
             }
         }
     }
